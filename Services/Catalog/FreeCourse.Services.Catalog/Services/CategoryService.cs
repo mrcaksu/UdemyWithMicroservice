7df8c32737d96bb2682e8a390b33f43f538288e8
mrcaksu61 <aksumuhammet61.ma@gmail.com>
2022-11-06 13:10:48 +0300

    Add project files.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/CategoryService.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Models;
+using FreeCourse.Services.Catalog.Settings;
+using FreeCourse.Shared.Dtos;
+using MongoDB.Driver;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+    public class CategoryService:ICategoryService
+    {
+        private readonly IMongoCollection<Category> _categoryColleciton;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IMapper mapper,IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+            _categoryColleciton = database.GetCollection<Category>(databaseSettings.CategoryCollectionName); 
+            _mapper = mapper;
+        }
+        public async Task<Response<List<CategoryDto>>> GetAllAsync()
+        {
+            var categories = await _categoryColleciton.Find(category => true).ToListAsync();
+            return Response<List<CategoryDto>>.Success(_mapper.Map<List<CategoryDto>>(categories), 200);
+        }
+        public async Task<Response<CategoryDto>> CreateAsync(CategoryDto categoryDto)
+        {
+            var category = _mapper.Map<Category>(categoryDto);
+            await _categoryColleciton.InsertOneAsync(category);
+            return Response<CategoryDto>.Success(_mapper.Map<CategoryDto>(category),200);
+        }
+        public async Task<Response<CategoryDto>> GetByIdAsync(string id)
+        {
+            var category = await _categoryColleciton.Find<Category>(x => x.Id == id).FirstOrDefaultAsync();
+            if(category == null)
+            {
+                return Response<CategoryDto>.Fail("Category not found", 404);
+            }
+            return Response<CategoryDto>.Success(_mapper.Map<CategoryDto>(category), 200);
+        }
+    }
+}
---
+++ Shared/FreeCourse.Shared/Dtos/Response.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace FreeCourse.Shared.Dtos
+{
+    public class Response<T>
+    {
+        public T Data { get; private set; }
+
+        [JsonIgnore]
+        public int StatusCode { get; private set; }
+
+        [JsonIgnore]
+        public bool IsSuccesful { get; private set; }
+
+        public List<String> Errors { get; set; }
+
+        //Static Factory Method=> statik metodlarla beraber geriye yeni bir nesne dönüyorsanız böyle denir
+        public static Response<T> Success(T data, int statusCode)
+        {
+            return new Response<T> { Data = data, StatusCode = statusCode, IsSuccesful=true };
+        }
+        public static Response<T> Success (int statusCode)
+        {
+            return new Response<T> { Data=default, StatusCode = statusCode, IsSuccesful=true };
+        }
+        public static Response<T> Fail (List<String> errors,int statusCode)
+        {
+            return new Response<T>
+            {
+                Errors = errors,
+                StatusCode = statusCode,
+                IsSuccesful = false
+            };
+        }
+        public static Response<T> Fail (string error,int statusCode)
+        {
+            return new Response<T> { Errors = new List<string>() { error }, StatusCode = statusCode, IsSuccesful = false }; 
+        }
+    }
+}
